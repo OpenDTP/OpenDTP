923458e4ac929190563e00cb8fc7ac97c7be4a6c
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2014-07-01 01:30:53 +0200

    Seulement la page login est accesible pour un guest

--- app/modules/core/routes.php
+++ app/modules/core/routes.php
@@ -11,16 +11,33 @@
 |
 */
 
-
-Route::get('/', function () {
-    return View::make('core::site.editor.dashboard');
+Route::filter('oauth', function () {
+    if (!Session::get('user.token')) {
+        return Redirect::to('user/login');
+    }
 });
 
-Route::get('editor/{path?}', function ($path = 'dashboard') {
+Route::group(array('before' => 'oauth'), function () {
 
-    return View::make('core::site.editor.' . $path);
+    Route::get('/', function () {
+        return View::make('core::site.editor.dashboard');
+    });
+
+    Route::get('editor/{path?}', function ($path = 'dashboard') {
+
+        return View::make('core::site.editor.' . $path);
+    });
+
+    Route::get('user/logout', function () {
+        Session::flush();
+        return View::make('core::site.editor.dashboard');
+    });
+    Route::pattern('id', '[0-9]+');
+    Route::get('{model}/{id}/show', 'App\Modules\Core\Controllers\UserController@show');
+    Route::get('{model}/{id}/edit', 'App\Modules\Core\Controllers\UserController@getEdit');
+    //Route::post('{model}/{id}/edit', 'App\Modules\Core\Controllers\UserController@store');
+    Route::put('{model}/{id}/edit', 'App\Modules\Core\Controllers\UserController@update');
 });
-
 
 Route::post('user/login', 'App\Modules\Core\Controllers\LoginController@login');
 Route::get('user/login', function () {
@@ -30,12 +47,3 @@ Route::get('user/login', function () {
         return View::make('core::site.user.login');
     }
 });
-Route::get('user/logout', function () {
-    Session::flush();
-    return View::make('core::site.editor.dashboard');
-});
-Route::pattern('id', '[0-9]+');
-Route::get('{model}/{id}/show', 'App\Modules\Core\Controllers\UserController@show');
-Route::get('{model}/{id}/edit', 'App\Modules\Core\Controllers\UserController@getEdit');
-//Route::post('{model}/{id}/edit', 'App\Modules\Core\Controllers\UserController@store');
-Route::put('{model}/{id}/edit', 'App\Modules\Core\Controllers\UserController@update');
