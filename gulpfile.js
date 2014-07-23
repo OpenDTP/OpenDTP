192251a1a11c4846e1d704e7e0a97bfc5d5fcdc1
Michael FORASTE <michael.foraste@gmail.com>
2014-07-23 15:22:09 +0200

    ref#182 working gulp + bower for external components

--- gulpfile.js
+++ gulpfile.js
@@ -6,66 +6,90 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
+var csso = require('gulp-csso');
+var less = require('gulp-less');
+var rename = require('gulp-rename');
 var del = require('del');
 
-var paths = {
-    sources: {
-        'app/modules': {
-            'scripts' : [ 'tt./**/*.js', '!./**/*.min.js' ],
-            'less' : [ '/**/*.less' ]
-        }
-    },
-    images: 'client/img/**/*'
+// compiled paths for gulp by modules
+var paths = {};
+
+// modules path definition
+var modules = {
+    'app/modules': {
+        'scripts' : [ '**/*.js' ],
+        'stylesheets' : [ '**/*.less' ],
+        'images' :  [ '**/*.(jpg|jpeg|png|ico)' ]
+    }
 };
 
-// Not all tasks need to use streams
-// A gulpfile is just another node program and you can use all packages available on npm
-//gulp.task('clean', function(cb) {
-    // You can use multiple globbing patterns as you would with `gulp.src`
-//    del(['build'], cb);
-//});
+(function () {
+    var processModules = function(source, modulesList) {
+        var root;
 
-var processModules = function(source, modules) {
-    var processed = [];
-
-    for (var key in modules) {
-        processed.concat(
-            gulp.src(paths.sources[source].scripts, {'root': process.cwd() + '/' + source + '/' + modules[key]})
-                .pipe(sourcemaps.init())
-                .pipe(uglify())
-                .pipe(concat(modules[key] + '.min.js'))
-                .pipe(sourcemaps.write())
-                .pipe(gulp.dest('build/js'))
-        );
+        for (var key in modulesList) {
+            root = source + '/' + modulesList[key];
+            paths[modulesList[key]] = {};
+            for (var element in modules[source]) {
+                paths[modulesList[key]][element] = modules[source][element].map(function (path) { return root + '/' + path });
+            }
+        }
     }
 
-    return processed;
-}
+    for (var path in modules) {
+        processModules(path, fs.readdirSync(path));
+    }
+
+})();
+
 
 gulp.task('scripts', function() {
-
-    var processed = [];
-
-    for (var path in paths.sources) {
-        fs.readdir(path, function (err, files) {
-           processed.concat(processModules(path, files));
-        });
+    for (module in paths) {
+        gulp.src(paths[module].scripts)
+            .pipe(sourcemaps.init())
+            .pipe(uglify())
+            .pipe(concat(module + '.min.js'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('public/js/' + module));
     }
-    // Minify and copy all JavaScript (except vendor scripts)
-    // with sourcemaps all the way down
-//    return gulp.src(['app/modules/**/*.js'])
-//        .pipe(sourcemaps.init())
-//        .pipe(uglify())
-//        .pipe(concat('application.min.js'))
-//        .pipe(sourcemaps.write())
-//        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('stylesheets', function() {
+    for (module in paths) {
+        gulp.src(paths[module].stylesheets)
+            .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(csso())
+            .pipe(rename(module + '.min.css'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('public/css/' + module));
+    }
+});
+
+gulp.task('images', function() {
+    for (module in paths) {
+        gulp.src(paths[module].images)
+            .pipe(imagemin({optimizationLevel: 5}))
+            .pipe(gulp.dest('public/images/' + module));
+    }
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.images, ['images']);
+    for (module in paths) {
+        gulp.watch(paths[module].scripts, ['scripts']).on('change', function (event) {
+            console.log('Event type: ' + event.type);
+            console.log('Event path: ' + event.path);
+        });
+        gulp.watch(paths[module].stylesheets, ['stylesheets']).on('change', function (event) {
+            console.log('Event type: ' + event.type);
+            console.log('Event path: ' + event.path);
+        });
+        gulp.watch(paths[module].images, ['images']).on('change', function (event) {
+            console.log('Event type: ' + event.type);
+            console.log('Event path: ' + event.path);
+        });
+    }
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'stylesheets', 'images']);
