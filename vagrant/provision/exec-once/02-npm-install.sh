e42921986d4964ff27903595e6864a437c9f5794
Michael FORASTE <michael.foraste@gmail.com>
2014-07-23 18:03:35 +0200

    adding nodejs + gulp + bower

---
+++ vagrant/provision/exec-once/02-npm-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing npm packages"
+
+cd /data
+npm install
---
+++ vagrant/provision/exec-once/03-bower-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing bower dependencies"
+
+cd /data
+yes 'no' | bower install --allow-root
