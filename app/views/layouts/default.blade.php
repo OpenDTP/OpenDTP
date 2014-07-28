4937eda35e3e2508ee4725e22b8241d4cfd21c1c
Michael FORASTE <michael.foraste@gmail.com>
2014-07-28 21:16:33 +0200

    Dashboard style refactoring

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -27,12 +27,12 @@
 <!--		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{{ asset('assets/ico/apple-touch-icon-114-precomposed.png') }}}">-->
 <!--		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}}">-->
 <!--		<link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}}">-->
-		<link rel="shortcut icon" href="{{{ asset('images/core/favicon.png') }}}">
+		<link rel="shortcut icon" href="{{{ asset('images/shared/favicon.png') }}}">
 	</head>
 
 	<body>
 		<div id="wrap">
-			<div class="navbar navbar-default navbar-inverse navbar-fixed-top">
+			<div class="navbar navbar-default navbar-fixed-top">
 				 <div class="container">
 						<div class="navbar-header">
 							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
