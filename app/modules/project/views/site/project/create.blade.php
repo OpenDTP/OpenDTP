f9be1d692fff703b25dbf69d901ba79fc27fbe62
Michael FORASTE <michael.foraste@gmail.com>
2014-08-25 10:53:56 +0200

    project creation form

--- app/modules/project/views/site/project/create.blade.php
+++ app/modules/project/views/site/project/create.blade.php
@@ -16,6 +16,37 @@
     </div>
 </div>
 <div class="project-create">
+    {{ Form::open(['url' => '/project', 'role' => 'form', 'class' => 'form validate']) }}
+        <div class="form-group">
+            <div class="row">
+                <div class="col-md-4">
+                    {{ Form::select('company',
+                        ['company', 'test', 'test2'],
+                        null,
+                        [ 'class' => 'form-control', 'id' => 'company', 'placeholder' => 'company' ]
+                    ) }}
+                </div>
+                <div class="col-md-4">
+                    <div class="input-group">
+                        {{ Form::text('name', null, [
+                            'class' => 'form-control', 'id' => 'name', 'placeholder' => 'name'
+                        ]) }}
+                    </div>
+                    <div class="input-group">
+                        {{ Form::textarea('description', null, [
+                            'class' => 'form-control', 'id' => 'description', 'placeholder' => 'project description'
+                        ]) }}
+                    </div>
+                    <div class="btn-group right">
+                        {{ Form::submit('Create', array('class' => 'btn btn-success')) }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                </div>
+            </div>
+        </div>
+    {{ Form::close() }}
+</div>
 </div>
 @stop
 @section('script')
